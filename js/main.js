ffab8696a0299a7c67f2c5657e66716694e154c1
Tusar <asktusar@gmail.com>
2013-12-09 15:29:03 +0530

    used html5 local storage

--- js/main.js
+++ js/main.js
@@ -1,48 +1,53 @@
 $(document).ready(function(){
-	$('#login_btn').click(function(){
-		$('#loading').show();
-		var form = $("#loginForm");    
-		var e = $("#email").val();
-		var p = $("#password").val();
-		//console.log("click");
-		if(e != "" && p != "") {
-			$.ajax({ 
-				 type: 'POST', 
-				 url: 'http://pixelmarketing.biz/clientservertest/UserLogin.php', 
-				 crossDomain: true,
-				 data:  {email: e, password :p},
-				 dataType: 'json', 
-				 async: false,
-				 success: function (response){ 
-					$('#loading').hide();
-					//alert ("response"); 
-					if (response.success) { 
-						alert("you're logged in");
-						window.localStorage["email"] = e;
-						window.localStorage["password"] = p;        
-						$("#loginPage").hide();
-						$("#page2").show();
-					} 
-					else {
+	if (localStorage.getItem("email") === null) {
+		$('#login_btn').click(function(){
+			$('#loading').show();
+			var form = $("#loginForm");    
+			var e = $("#email").val();
+			var p = $("#password").val();
+			//console.log("click");
+			if(e != "" && p != "") {
+				$.ajax({ 
+					 type: 'POST', 
+					 url: 'http://pixelmarketing.biz/clientservertest/UserLogin.php', 
+					 crossDomain: true,
+					 data:  {email: e, password :p},
+					 dataType: 'json', 
+					 async: false,
+					 success: function (response){ 
 						$('#loading').hide();
-						alert("Your login failed! Invalid Credentials");
-						//window.location("main.html");
-					}
-
-			 },
-						 error: function(error){
+						//alert ("response"); 
+						if (response.success) { 
+							alert("you're logged in");
+							window.localStorage["email"] = e;
+							window.localStorage["password"] = p;        
+							$("#loginPage").hide();
+							$("#page2").show();
+						} 
+						else {
 							$('#loading').hide();
-							alert('Could not connect to the database' + error);
-							//window.location = "main.html";
+							alert("Your login failed! Invalid Credentials");
+							//window.location("main.html");
 						}
-			}); 
+
+				 },
+							 error: function(error){
+								$('#loading').hide();
+								alert('Could not connect to the database' + error);
+								//window.location = "main.html";
+							}
+				}); 
+		}
+		else {
+			//if the email and password is empty
+			$('#loading').hide();
+			alert("You must enter email and password");
+		
+		}
+		return false;
+		});
+	} else{
+		$("#loginPage").hide();
+		$("#page2").show();
 	}
-	else {
-		//if the email and password is empty
-		$('#loading').hide();
-		alert("You must enter email and password");
-	
-	}
-	return false;
-	});
 });
